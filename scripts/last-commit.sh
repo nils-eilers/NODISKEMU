1d12b40d9f6ec7d57648fdd90a8942a6d88f8c07
nils-eilers <nils.eilers@gmx.de>
2015-06-08 01:09:55 +0200

    Add some error handling to last-commit.sh
    If git is not available or the source directory is not
    a git repo, the current date YYMMDD is used as a
    date of the latest commit and NOGIT for the hash of
    the latest commit.

--- scripts/last-commit.sh
+++ scripts/last-commit.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
+# -d : print date of latest git commit
+# -h : print short hash of latest commit
+# If we're lost in something that isn't a git repo or git is
+# not installed, output these replacements:
+# -d : current date as YYMMDD
+# -h : NOGIT
+
 if [ $# -eq 0 ] ; then
    printf "Info about the latest commit:\n\t-d\tDate\n\t-h\tHash value\n"
    exit 1
 fi
 case "$1" in
--d) git log --pretty=format:"%ai" | head -n1 | cut -c 1-4,6-7,9-10
+-d) git --version >/dev/null 2>&1 || { date "+%Y%m%d"; exit 0; }
+    if git rev-parse --git-dir > /dev/null 2>&1; then
+      git log --pretty=format:"%ai" | head -n1 | cut -c 1-4,6-7,9-10
+    else
+      date "+%Y%m%d"
+    fi
     ;;
--h) git log --pretty=format:"%h" | head -n1 | awk '{print toupper($0)}'
+
+-h) git --version >/dev/null 2>&1 || { echo NOGIT; exit 0; }
+    if git rev-parse --git-dir > /dev/null 2>&1; then
+      git log --pretty=format:"%h" | head -n1 | awk '{print toupper($0)}'
+    else
+      echo NOGIT
+    fi
     ;;
 *) echo "Illegal option '$1'"
     ;;
