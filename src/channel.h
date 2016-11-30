7dfe6db471d2b016cda71a3554b15edced07993c
nils-eilers <nils.eilers@gmx.de>
2016-11-30 23:50:17 +0100

    Import emily's SPSP functions
    SPSP = Serial PET/Server Protocol
    
    Have a look at doc/Serial-protocol.odt
    for a further description.

---
+++ src/channel.h
@@ -0,0 +1,67 @@
+/*-
+ * Copyright (c) 2015 Nils Eilers. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+#include "defaults.h"
+
+#define COMMAND_CHANNEL 15
+#define STATUS_CHANNEL  15
+#define BUF_LEN 256
+
+struct Buffer
+{
+   uint8_t  Device;             // zero if buffer is not used
+   uint8_t  SecondaryAddress;   // aka channel
+   uint16_t ptr;                // index to next r/w position in buffer
+   uint16_t len;                // valid bytes in buffer
+   uint16_t rec;                // record # of relative file
+   uint8_t  Mode;               // access mode
+   bool     SendEoi;            // send Data[EoiPosition] with EOI
+   bool     LoadRequired;       // call load() before next access
+   uint8_t  EoiPosition;
+   uint8_t  Data[BUF_LEN];
+   void     (*Load)(struct Buffer *self);
+};
+
+extern struct Buffer Buffers[CONFIG_MAX_BUFFERS];
+
+// Get next character but do not advance pointers because
+// transmission to CBM may get interrupted by ATN
+int channel_Get(uint8_t AddressedDevice, uint8_t sa);
+
+// Confirm transmission: advance buffer pointers
+void channel_Got(uint8_t device, uint8_t sa);
+void channel_Put(char c, uint8_t AddressedDevice, uint8_t sa);
+
+void channel_Open(char *s, uint8_t AddressedDevice, uint8_t sa);
+void channel_Close(uint8_t AddressedDevice, uint8_t sa);
+
+struct Buffer *channel_BufferSearch(uint8_t device, uint8_t sa);
+struct Buffer *channel_BufferSearchOrAllocate(uint8_t device, uint8_t sa);
+void channel_LoadBuffers(void);
+void channel_FreeBuffer(uint8_t device, uint8_t sa);
+void channel_OpenDirectBuffer(uint8_t device, uint8_t sa, char *filename);
---
+++ src/spsp.h
@@ -0,0 +1,90 @@
+/*-
+ * Copyright (c) 2015 Nils Eilers. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "channel.h"
+
+extern bool RemoteMode;
+
+
+enum SPSP_CMD_fields
+{
+   PET_CMD,
+   PET_FA,
+   PET_LOG = PET_FA,
+   PET_SA,
+   PET_DATA
+};
+
+enum SPSP_commands
+{
+   CMD_LOG,
+   CMD_OPEN,
+   CMD_CLOSE,
+   CMD_GET_BLOCK,
+   CMD_PUT_BLOCK,
+   CMD_GET_RECORD,
+   CMD_PUT_RECORD,
+   CMD_READ,
+   CMD_ADVANCE,
+   CMD_WRITE,
+   CMD_RESET,
+   CMD_SERV,
+   CMD_BUS_CMD,
+   CMD_BUS_DATA
+};
+
+enum CONF_commands
+{
+   CONF_DONE,
+   CONF_ADD_DEVICE_NUMBER,
+   CONF_RM_DEVICE_NUMBER,
+   CONF_SNIFF
+};
+
+
+void spsp_SendEscEot(void);
+void spsp_Check(void);
+void spsp_Connect(void);
+void spsp_PullConfCommands(void);
+uint8_t spsp_GetAckStatus(uint8_t device);
+uint8_t spsp_GetFullStatus(uint8_t device);
+void spsp_ListenLoop(uint8_t action, uint8_t sa);
+uint8_t spsp_OpenFile(uint8_t sa, char *filename);
+void spsp_SendCommand(char *command, uint8_t len);
+void spsp_Advance(uint8_t device, uint8_t sa);
+void spsp_Message(char *message);
+void spsp_LoadBuffer(struct Buffer *buf);
+void spsp_UserCommand(char *command);
+void spsp_BlockCommand(char *command, uint8_t len);
+void spsp_PositionCommand(uint8_t *command, uint8_t len);
+void spsp_GetBlock(struct Buffer *buf, uint8_t device, uint8_t drive, uint8_t t, uint8_t s);
+void spsp_PutBlock(struct Buffer *buf, uint8_t device, uint8_t drive, uint8_t t, uint8_t s);
+void spsp_GetRecord(struct Buffer *buf);
+void spsp_PutRecord(struct Buffer *buf);
+
